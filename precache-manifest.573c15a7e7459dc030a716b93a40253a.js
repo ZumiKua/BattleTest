296fbbce0e803f5e3f6e46023056d25d1ddd7c25
Zumi Kua <wlzqkwd@Gmail.com>
2019-06-09 01:36:32 +0800

    Updates

---
+++ precache-manifest.573c15a7e7459dc030a716b93a40253a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302e36068000857f8eb9149ecd37ba54",
+    "url": "/BattleTest/index.html"
+  },
+  {
+    "revision": "95c31f4fd92dded775cf",
+    "url": "/BattleTest/static/css/main.62974792.chunk.css"
+  },
+  {
+    "revision": "b5c03343c50dd22e6bec",
+    "url": "/BattleTest/static/js/2.0f9afa02.chunk.js"
+  },
+  {
+    "revision": "95c31f4fd92dded775cf",
+    "url": "/BattleTest/static/js/main.0f39d82b.chunk.js"
+  },
+  {
+    "revision": "58ce3e9da0c44de6603c",
+    "url": "/BattleTest/static/js/runtime~main.82ad675c.js"
+  }
+]);
